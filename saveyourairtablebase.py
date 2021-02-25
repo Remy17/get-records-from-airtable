42edc779b4f4f5b725889bb7c85526c35ad8353f
Rémy Ramadour <remyramadour@gmx.fr>
2021-02-25 21:28:19 +0100

    Update saveyourairtablebase.py

--- saveyourairtablebase.py
+++ saveyourairtablebase.py
@@ -1,30 +1,24 @@
 import json
-import os, sys
+import os
 from airtable import Airtable
+import config as cfg
 
 if __name__ == "__main__":
 
-    _baseKey = "appXXXXX"
-    _table_name = "YYYYY"
-    _apiKey = "apiZZZZZ"
-    _view = "Grid view"
-
     # get your airtable instance
-    #airtable = Airtable( basekey = _baseKey, table_name = _table_name, api_key = _apiKey)
-    airtable = Airtable('appS4HDhbgxbzOXco', 'Quiz', api_key = 'keyTv9YoTufkmEn6e')
+    airtable = Airtable(cfg.baseKey, cfg.table_name, cfg.apiKey)
 
     # get record from a certain view
-    records = airtable.get_all(view = _view, maxRecords = 100)
+    records = airtable.get_all(view = cfg.view, maxRecords = 100)
 
     if not os.path.isdir('records') :
-        os.mkdir( "records", 0o755 )
+        os.mkdir("records", 0o755 )
 
     for record in records :
 
         # get fields from record
         fields = record['fields']
 
+        # save them in a file
         with open('./records/record_' + record['id'] + '.json', 'w') as outfile:
             json.dump(fields, outfile)
-
-        #airtable.update(record['id'], {'URL Typeform' : data['_links']['display']})
